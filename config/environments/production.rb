25f713697e5c7b28dbb0f055a0961de39156c3cd
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-09-26 05:14:35 +0900

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Kfarm::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -77,5 +77,5 @@ Kfarm::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => 'farmfarmmentor.org' }
+  config.action_mailer.default_url_options = { :host => 'kfarm.moncl.net' }
 end
