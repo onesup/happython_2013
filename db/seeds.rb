723479cf0ff25a663505f3f54b5349c692f167f8
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-09-28 05:33:49 +0900

    seed mock data

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,8 +16,26 @@ User.first.update( role: Role.with_name(:admin) )
 puts "created #{User.first.email} and that made administer"
 
 jobs = ["사과 수확","배 수확","포도 수확","복숭아 수확","멜론 수확","깻잎 수확","호박 수확","옥수수 수확"]
-
+answers = ["사과 수확","배 수확","포도 수확","복숭아 수확","멜론 수확","깻잎 수확","호박 수확","옥수수 수확"]
+notices = ["사과 수확","배 수확","포도 수확","복숭아 수확","멜론 수확","깻잎 수확","호박 수확","옥수수 수확"]
+questions = ["사과 수확","배 수확","포도 수확","복숭아 수확","멜론 수확","깻잎 수확","호박 수확","옥수수 수확"]
+reviews = ["사과 수확","배 수확","포도 수확","복숭아 수확","멜론 수확","깻잎 수확","호박 수확","옥수수 수확"]
+users = ["사과 수확","배 수확","포도 수확","복숭아 수확","멜론 수확","깻잎 수확","호박 수확","옥수수 수확"]
 jobs.each do |job|
   Job.create(title: job)
-  puts Job.last.title
+end
+answers.each do |answer|
+  Answer.create(title: answer)
+end
+notices.each do |notice|
+  Notice.create(title: notice)
+end
+questions.each do |question|
+  Question.create(title: question)
+end
+reviews.each do |review|
+  Review.create(title: review)
+end
+users.each do |user|
+  User.create(name: user)
 end
