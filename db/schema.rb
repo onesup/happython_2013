5e8571f629fa541ffcb1442a92f38810e45ce28c
innoceive <innoceive@gmail.com>
2013-09-28 04:13:01 +0900

    레이아웃 작업 잔여

--- db/schema.rb
+++ db/schema.rb
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20130913163424) do
     t.datetime "updated_at"
   end
 
-  add_index "applications", ["job_id"], name: "index_applications_on_job_id", using: :btree
-  add_index "applications", ["mentee_id"], name: "index_applications_on_mentee_id", using: :btree
+  add_index "applications", ["job_id"], name: "index_applications_on_job_id"
+  add_index "applications", ["mentee_id"], name: "index_applications_on_mentee_id"
 
   create_table "jobs", force: true do |t|
     t.integer  "mentor_id"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20130913163424) do
     t.datetime "updated_at"
   end
 
-  add_index "jobs", ["mentor_id"], name: "index_jobs_on_mentor_id", using: :btree
+  add_index "jobs", ["mentor_id"], name: "index_jobs_on_mentor_id"
 
   create_table "roles", force: true do |t|
     t.string   "name",        null: false
@@ -72,9 +72,9 @@ ActiveRecord::Schema.define(version: 20130913163424) do
     t.string   "uid"
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["unlock_token"], name: "index_users_on_unlock_token", unique: true
 
 end
