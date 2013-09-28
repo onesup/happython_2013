6093c18fa86411dc247cb6aca8cb35324d416f9c
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-09-29 04:43:49 +0900

    job schema 변경

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,5 @@ class Job < ActiveRecord::Base
   belongs_to :mentor, class_name: "User"
   has_many :applications
   has_many :mentees, through: :applications
+  serialize :time
 end
--- db/migrate/20130927212619_create_jobs.rb
+++ db/migrate/20130927212619_create_jobs.rb
@@ -5,11 +5,18 @@ class CreateJobs < ActiveRecord::Migration
       t.string :category
       t.string :title
       t.text :content
-      t.text :time
+      t.datetime :started_at
+      t.datetime :finished_at
+      t.integer :days
+      t.integer :start_time
+      t.integer :finish_time
+      t.integer :working_time
+      t.integer :learning_time
       t.string :level
       t.integer :workers_count
       t.integer :pay
       t.string :address
+      t.string :plays
 
       t.timestamps
     end
