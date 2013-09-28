82b1925a3bc32ef5e3bce818de31aab420ba96df
Claude Yun <byoungkook.yu@gmail.com>
2013-09-28 06:39:57 +0000

    관리자페이지 퍼블리싱 (메인데시보드, 사용자메뉴)

---
+++ app/assets/javascripts/faq.js
@@ -0,0 +1,10 @@
+/*
+$(document).ready(function() {
+  // 상세보기
+  $('div.faq_wrapper > ul.list > li').click(function() {
+    $(this).find('span.detail').toggleClass('show');
+    $(this).find('span.pointer').toggleClass('down');
+  });
+
+});
+*/
