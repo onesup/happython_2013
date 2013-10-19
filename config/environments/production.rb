6b954bbe310cad987ccb805a62d790afc57bcdbc
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-10-20 00:51:26 +0900

    deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Kfarm::Application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_assets = false
   config.assets.js_compressor = :uglifier
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.assets.version = '1.0'
   config.log_level = :debug
