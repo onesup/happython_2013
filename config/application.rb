55cd5c92a12690a48eb01f9ecc982fb89b0204ed
Wonsup Lee이원섭 <onesup.lee@gmail.com>
2013-10-19 19:54:51 +0900

    vendor css js 정리

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module Kfarm
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.time_zone = 'Asia/Seoul'
+    config.assets.precompile = ['application.js', 'application.css', 'admin.js', 'admin.css']
   end
 end
